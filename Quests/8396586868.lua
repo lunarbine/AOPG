1c83a5dfbb209542c98abdcbf08b07a04771e8f5
lunarbine <lunarbine@outlook.com>
2026-01-13 00:13:44 -0600

    Create 8396586868.lua

---
+++ Quests/8396586868.lua
@@ -0,0 +1,22 @@
+return {
+    {
+        questDummy = 'Quest Dummy 1',
+        mobs = { 'Bandit', 'Noob', 'Monkey', 'Gorilla', 'Bandit Leader' }
+    },
+    {
+        questDummy = 'Quest Dummy 2',
+        mobs = { 'Marine' }
+    },
+    {
+        questDummy = 'Quest Dummy 3',
+        mobs = { 'Pirate' }
+    },
+    {
+        questDummy = 'Quest Dummy 4',
+        mobs = { 'Fishman', 'Stronger Fishman' }
+    },
+    {
+        questDummy = 'Quest Dummy 5',
+        mobs = { 'Buggy Pirate' }
+    }
+}
