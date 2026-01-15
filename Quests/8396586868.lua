ade1b71430e75b87f59dc3511c3974219b4cc7c9
lunarbine <lunarbine@outlook.com>
2026-01-14 20:09:02 -0600

    Update 8396586868.lua

--- Quests/8396586868.lua
+++ Quests/8396586868.lua
@@ -1,21 +1,27 @@
+--// First Sea
 return {
     {
+        island = 'Starter Island',
         questDummy = 'Quest Dummy 1',
         mobs = { 'Bandit', 'Noob', 'Monkey', 'Gorilla', 'Bandit Leader' }
     },
     {
+        island = 'Clam town',
         questDummy = 'Quest Dummy 2',
         mobs = { 'Marine' }
     },
     {
+        island = 'Wilderness',
         questDummy = 'Quest Dummy 3',
         mobs = { 'Pirate' }
     },
     {
+        island = 'Shark town',
         questDummy = 'Quest Dummy 4',
         mobs = { 'Fishman', 'Stronger Fishman' }
     },
     {
+        island = 'Citrus village',
         questDummy = 'Quest Dummy 5',
         mobs = { 'Buggy Pirate' }
     }
