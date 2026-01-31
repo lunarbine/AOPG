0918a5151196332710dd6d9508c7ddfedaef2d0c
lunarbine <lunarbine@outlook.com>
2026-01-31 16:23:35 -0600

    Create 11216777504.lua

---
+++ Quests/11216777504.lua
@@ -0,0 +1,8 @@
+--// Multiverse Sea
+return {
+    {
+        island = 'Hueco Mundo',
+        questDummy = '',
+        mobs = { 'Hollow', 'Espada', 'Aizen', 'Zangetsu', 'Hollow Ichigo' }
+    }
+}
