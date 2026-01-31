64f71bf2c1e5da3ff6e8378f9622b9ab46dfe0b1
lunarbine <lunarbine@outlook.com>
2026-01-31 17:53:37 -0600

    Update 8396586868.lua

--- Quests/8396586868.lua
+++ Quests/8396586868.lua
@@ -16,6 +16,10 @@ return {
         mobs = { 'Pirate' }
     },
     {
+        island = 'Wilderness',
+        mobs = { 'Snakeman Strawboy' }
+    },
+    {
         island = 'Shark town',
         questDummy = 'Quest Dummy 4',
         mobs = { 'Fishman', 'Stronger Fishman' }
@@ -24,5 +28,99 @@ return {
         island = 'Citrus village',
         questDummy = 'Quest Dummy 5',
         mobs = { 'Buggy Pirate' }
+    },
+    {
+        island = 'Sky Islands',
+        questDummy = 'Quest Dummy 6',
+        mobs = { 'Skypiean', 'Shandian', 'Thunder God' }
+    },
+    {
+        island = 'Desert Island',
+        questDummy = 'Quest Dummy 7',
+        mobs = { 'Desert Bandit', 'Desert Bandit Leader', 'Alligator' }
+    },
+    {
+        island = 'Arena',
+        questDummy = 'Quest Dummy 8',
+        mobs = { 'The Boss' }
+    },
+    {
+        island = 'Marine Headquarters',
+        questDummy = 'Quest Dummy 9',
+        mobs = { 'Strong Marine', 'Oyaji' }
+    },
+    {
+        island = 'Maple village',
+        questDummy = 'Quest Dummy 10',
+        mobs = { 'Aesop' }
+    },
+    {
+        island = 'Sea Prison',
+        questDummy = 'Quest Dummy 11',
+        mobs = { 'Poison Guard', 'Impel Guard' }
+    },
+    {
+        island = 'Flamingo\'s Island',
+        questDummy = 'Quest Dummy 12',
+        mobs = { 'Goon', 'Heavenly Demon' }
+    },
+    {
+        island = 'Strawhat Island',
+        questDummy = 'Quest Dummy 13',
+        mobs = { 'Mountain Bandit', 'Strawboy' }
+    },
+    {
+        island = 'Ice Island',
+        questDummy = 'Quest Dummy 14',
+        mobs = { 'Sword Marine', 'Trained Marine', 'Smokey' }
+    },
+    {
+        island = 'Frostburn',
+        questDummy = 'Quest Dummy 15',
+        mobs = { 'Gas Clown', 'Magma Admiral' }
+    },
+    {
+        island = 'Phoenix Nest',
+        questDummy = 'Quest Dummy 16',
+        mobs = { 'Pineapple Head', 'Donut Man' }
+    },
+    {
+        island = 'Raid Kingdom',
+        questDummy = 'Quest Dummy 17',
+        mobs = { 'Germa Soldier', 'Germa Lieutenant', 'Germa General', 'Germa Sergeant Major', 'Uno', 'Doso', 'Sero', 'Silent Noir Treso' }
+    },
+    {
+        island = 'Buggy Island',
+        questDummy = 'Quest Dummy 18',
+        mobs = { 'Strong Buggy Pirate', 'Buggy General', 'Captain Clown' }
+    },
+    {
+        island = 'New Alabasta',
+        questDummy = 'Quest Dummy 20',
+        mobs = { 'Sand General', 'Elite Desert Bandit', 'Awakened Alligator' }
+    },
+    {
+        island = 'Ghost Town',
+        questDummy = 'Quest Dummy 22',
+        mobs = { 'Zombie', 'Skeleton', 'Ghost', 'Rotten Pumpkin', 'Shadow King' }
+    },
+    {
+        island = 'Land of Judgement',
+        questDummy = 'Quest Dummy 23',
+        mobs = { 'Rookie Heart Pirate', 'Heart Pirate', 'Ice Bear', 'Awakened Death Surgeon' }
+    },
+    {
+        island = '200M Island',
+        questDummy = 'Quest Dummy 24',
+        mobs = { 'Boss Studios Glitch', 'Boss Studios Intern', 'TheBoss Awakened' }
+    },
+    {
+        island = 'Draculas Domain',
+        mobs = { 'Living Armour', 'Plaque Beast', 'Vampiric Knight' }
+    },
+    {
+        island = 'DrumIsland',
+        questDummy = 'Quest Dummy 25',
+        mobs = { 'Snowman', 'Corrupted Snowman' }
     }
 }
