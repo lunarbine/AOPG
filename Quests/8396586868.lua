ddd3a48cddbdaf6450c17eaf5d5e8bb36799a332
lunarbine <lunarbine@outlook.com>
2026-01-31 18:39:47 -0600

    Update 8396586868.lua

--- Quests/8396586868.lua
+++ Quests/8396586868.lua
@@ -45,81 +45,81 @@ return {
         mobs = { 'The Boss' }
     },
     {
-        island = 'Marine Headquarters',
+        island = 'Marine Headquarters', -- 3/27/2022 8:17 PM
         questDummy = 'Quest Dummy 9',
         mobs = { 'Strong Marine', 'Oyaji' }
     },
     {
-        island = 'Maple village',
+        island = 'Maple village', -- 3/31/2022 5:20 AM
         questDummy = 'Quest Dummy 10',
         mobs = { 'Aesop' }
     },
     {
-        island = 'Sea Prison',
+        island = 'Sea Prison', -- 3/31/2022 7:42 PM
         questDummy = 'Quest Dummy 11',
         mobs = { 'Poison Guard', 'Impel Guard' }
     },
     {
-        island = 'Flamingo\'s Island',
+        island = 'Flamingo\'s Island', -- 4/1/2022 6:35 PM
         questDummy = 'Quest Dummy 12',
         mobs = { 'Goon', 'Heavenly Demon' }
     },
     {
-        island = 'Strawhat Island',
+        island = 'Strawhat Island', -- 4/3/2022 4:24 PM
         questDummy = 'Quest Dummy 13',
         mobs = { 'Mountain Bandit', 'Strawboy' }
     },
     {
-        island = 'Ice Island',
+        island = 'Ice Island', -- 4/4/2022 8:55 PM
         questDummy = 'Quest Dummy 14',
         mobs = { 'Sword Marine', 'Trained Marine', 'Smokey' }
     },
     {
-        island = 'Frostburn',
+        island = 'Frostburn', -- 4/5/2022 8:08 PM
         questDummy = 'Quest Dummy 15',
         mobs = { 'Gas Clown', 'Magma Admiral' }
     },
     {
-        island = 'Phoenix Nest',
+        island = 'Phoenix Nest', -- 4/13/2022 2:08 AM
         questDummy = 'Quest Dummy 16',
         mobs = { 'Pineapple Head', 'Donut Man' }
     },
     {
-        island = 'Raid Kingdom',
+        island = 'Raid Kingdom', -- 9/17/2022 4:59 PM
         questDummy = 'Quest Dummy 17',
         mobs = { 'Germa Soldier', 'Germa Lieutenant', 'Germa General', 'Germa Sergeant Major', 'Uno', 'Doso', 'Sero', 'Silent Noir Treso' }
     },
     {
-        island = 'Buggy Island',
+        island = 'Buggy Island', -- 9/23/2022 8:02 PM
         questDummy = 'Quest Dummy 18',
         mobs = { 'Strong Buggy Pirate', 'Buggy General', 'Captain Clown' }
     },
     {
-        island = 'New Alabasta',
+        island = 'New Alabasta', -- 9/29/2022 9:24 PM
         questDummy = 'Quest Dummy 20',
         mobs = { 'Sand General', 'Elite Desert Bandit', 'Awakened Alligator' }
     },
     {
-        island = 'Ghost Town',
+        island = 'Ghost Town', -- 10/30/2022 2:27 PM
         questDummy = 'Quest Dummy 22',
         mobs = { 'Zombie', 'Skeleton', 'Ghost', 'Rotten Pumpkin', 'Shadow King' }
     },
     {
-        island = 'Land of Judgement',
+        island = 'Land of Judgement', -- 12/10/2022 9:21 PM
         questDummy = 'Quest Dummy 23',
         mobs = { 'Rookie Heart Pirate', 'Heart Pirate', 'Ice Bear', 'Awakened Death Surgeon' }
     },
     {
-        island = '200M Island',
+        island = '200M Island', -- 3/23/2023 1:55 PM
         questDummy = 'Quest Dummy 24',
         mobs = { 'Boss Studios Glitch', 'Boss Studios Intern', 'TheBoss Awakened' }
     },
     {
-        island = 'Draculas Domain',
+        island = 'Draculas Domain', -- 10/28/2023 6:44 PM
         mobs = { 'Living Armour', 'Plaque Beast', 'Vampiric Knight' }
     },
     {
-        island = 'DrumIsland',
+        island = 'DrumIsland', -- 12/6/2025 5:26 PM
         questDummy = 'Quest Dummy 25',
         mobs = { 'Snowman', 'Corrupted Snowman' }
     }
