d3c2a1080f56edb62786fc68e8e12e864f8da863
lunarbine <lunarbine@outlook.com>
2026-01-31 18:22:17 -0600

    Update 11216777504.lua

--- Quests/11216777504.lua
+++ Quests/11216777504.lua
@@ -1,7 +1,33 @@
 --// Multiverse Sea
 return {
     {
-        island = 'Hueco Mundo',
+        island = 'Hueco Mundo', -- 9/24/2022 8:35 PM
         mobs = { 'Hollow', 'Espada', 'Aizen', 'Zangetsu', 'Hollow Ichigo' }
+    },
+    {
+        island = 'Goku\'s Island', -- 10/9/2022 11:46 AM
+        mobs = { 'Martial Artist', 'Saiyan Soldier' }
+    },
+    {
+        island = 'Goku\'s Island', -- 10/9/2022 11:46 AM
+        questDummy = 'Quest Dummy 1',
+        mobs = { 'Krillin', 'Goku', 'Super Saiyan Goku' }
+    },
+    {
+        island = 'Chain Domain', -- 12/3/2022 2:06 PM
+        mobs = { 'Chainsaw Xury' }
+    },
+    {
+        island = 'Konoha', -- 1/21/2023 3:21 PM
+        questDummy = 'Quest Dummy 2',
+        mobs = { 'Naruto', 'Transformed Naruto' }
+    },
+    {
+        island = 'Jujutsu', -- 5/10/2023 5:12 PM
+        mobs = { 'Sukuna', 'Divine Curse', 'King Of Curses' }
+    },
+    {
+        island = 'Ant Island', -- 5/11/2025 2:30 AM
+        mobs = { 'Berserk Ant King' }
     }
 }
